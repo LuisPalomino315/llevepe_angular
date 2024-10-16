b1f761f38a769139c3802b1d2dbcf79cf90b0a77
Abiiii1 <rutabigailsilvadiaz@gmail.com>
2024-10-16 13:02:49 -0500

    change

--- src/app/features/modelo/add-modelo/add-modelo.component.ts
+++ src/app/features/modelo/add-modelo/add-modelo.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ModeloService } from '../../../services/modelo.service';
+import { GlobalService } from 'src/app/services/global.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as toastr from 'toastr';
+import { CategoryService } from 'src/app/services/category.service';
+import 'select2';
 
 @Component({
   selector: 'app-add-modelo',
@@ -11,11 +14,15 @@ import * as toastr from 'toastr';
 })
 export class AddModeloComponent {
   addModeloForm: FormGroup;
+  categorys: any[] = [];
 
   constructor(
     private fb: FormBuilder,
     private modeloService: ModeloService,
+    private categoryService: CategoryService,
+    private global: GlobalService,
     private router: Router
+    
   ) {
     this.addModeloForm = this.fb.group({
       id_categoria: ['',[Validators.required, Validators.pattern('^[0-9]*$')]], // Validar como numérico
@@ -24,7 +31,23 @@ export class AddModeloComponent {
     });
   }
 
+  ngOnInit():void {
+    this.loadModelo();
+  }
+
+  ngAfterViewInit(): void {
+    const selectCategory = $('#id_categoria');
+    console.log(selectCategory)
+    // Detectar cambios en select2 y actualizar el formControl de Angular
+    selectCategory.on('change', (e: any) => {
+      const selectedValue = e.target.value;
+      console.log(selectedValue)
+      this.addModeloForm.get('id_categoria')?.setValue(selectedValue);
+    });
+  }
+
   onSubmit(): void {
+    console.log(this.addModeloForm)
     if (this.addModeloForm.valid) {
       this.modeloService.createModelo(this.addModeloForm.value).subscribe(
         response => {
@@ -64,6 +87,14 @@ export class AddModeloComponent {
         }
       }
     });
+  }
+
+  loadModelo(): void {
+    this.categoryService.getCategorys().subscribe(data => {
+      console.log(data); // Verificar los datos en la consola
+      this.categorys = data as any[];
+    });
+    this.global.initializeSelect2('Seleccione una categoria');
   }
 }
 
